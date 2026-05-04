ff853da3252fda8268c839d11f1fa1a238f8a907
Dr. Bruno Gustavo <brunogustavosa@gmail.com>
2026-05-04 08:53:41 -0300

    corrige supabase duplicado

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
